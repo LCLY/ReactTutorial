654f3e29e50ffb30001433f9e53e15c3a77b29ac
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-02 00:22:37 -0400

    refactor: added some text to render

--- udemy/src/index.js
+++ udemy/src/index.js
@@ -1,8 +1,13 @@
 //Import React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from 'react'; //ES2015 Modules system
+import ReactDOM from 'react-dom';
+
+//CommonJS Modules use require import statement
 
 //Create a react component
-
+const App = () => {
+	return <div> Hi there! </div>;
+};
 
 //Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
