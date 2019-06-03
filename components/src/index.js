9fabb0f2bede5a2b7c9342eccc7dd39e603373c6
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-03 17:55:18 -0400

    Refactor: wrapping component in component

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker'; //get image from the faker library
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail
-				author="Sam"
-				timeAgo="Today at: 4PM"
-				commentText="Hey hey hey"
-				avatarPic={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Abu"
-				timeAgo="Today at: 5PM"
-				commentText="NANIDESUKA"
-				avatarPic={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author="Suzu"
-				timeAgo="Today at: 6PM"
-				commentText="Testing 1 2 3"
-				avatarPic={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at: 4PM"
+					commentText="Hey hey hey"
+					avatarPic={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Abu"
+					timeAgo="Today at: 5PM"
+					commentText="NANIDESUKA"
+					avatarPic={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author="Suzu"
+					timeAgo="Today at: 6PM"
+					commentText="Testing 1 2 3"
+					avatarPic={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
