cd6a5eb759a8695f544640f88db931f9db7ba7f7
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-03 00:34:17 -0400

    refactor: testing object calling in jsx

--- udemy/src/index.js
+++ udemy/src/index.js
@@ -5,18 +5,27 @@ import ReactDOM from 'react-dom';
 //CommonJS Modules use require import statement
 
 //Create a react component
+
+function test() {
+	return 'CLICK THIS!';
+} //can be used in jsx
+
 //in JSX use className instead of class
 const App = () => {
+	const buttonText = 'Click Me!';
+	const jsObject = { text: 'object' };
 	return (
 		<div>
-			<label className="label" for="name">
+			<label className="label" htmlFor="name">
 				Enter name:
 			</label>
 			<input id="name" type="text" />
-			<button style={{ backgroundColor: 'blue', color: 'white' }}> Submit </button>
+			<button style={{ backgroundColor: 'blue', color: 'white' }}> {jsObject.text} </button>
 		</div>
 	);
 };
+
+//curly braces in JSX return to refer to function or variable outside
 
 //Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
