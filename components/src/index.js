818001206a26bc9b35586763323a1727b53de7f0
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-03 17:42:45 -0400

    refactor: props data transfer

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail author="Sam" />
-			<CommentDetail author="Abu" />
-			<CommentDetail author="What" />
+			<CommentDetail
+				author="Sam"
+				timeAgo="Today at: 4PM"
+				commentText="Hey hey hey"
+				avatarPic={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author="Abu"
+				timeAgo="Today at: 5PM"
+				commentText="NANIDESUKA"
+				avatarPic={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author="Suzu"
+				timeAgo="Today at: 6PM"
+				commentText="Testing 1 2 3"
+				avatarPic={faker.image.avatar()}
+			/>
 		</div>
 	);
 };
