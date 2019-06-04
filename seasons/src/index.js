d7caf099b3ddbc3467187a31e2ddd27f0ebb9da9
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-04 15:40:31 -0400

    refactor: if statement on renders

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,12 +7,23 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		//initialize relevant value
-		this.state = { lat: null };
+		this.state = { lat: null, errorMessage: '' };
+
+		window.navigator.geolocation.getCurrentPosition(
+			position => this.setState({ lat: position.coords.latitude }),
+			err => this.setState({ errorMessage: err.message }),
+		);
+		//never assign the value directly to this.state e.g. this.state.lat = position.coords.latitude
 	}
 
 	render() {
-		window.navigator.geolocation.getCurrentPosition(position => console.log(position), err => console.log(err));
-		return <div>Latitude: </div>;
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>{this.state.errorMessage}</div>;
+		}
+		if (this.state.lat && !this.state.errorMessage) {
+			return <div>{this.state.lat}</div>;
+		}
+		return <div>loading</div>;
 	}
 }
 
