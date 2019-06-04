975c9d47bac13b87667748f3fe61a1798837d0ba
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-04 03:24:16 -0400

    feat: added geolocation api

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
 const App = () => {
+	window.navigator.geolocation.getCurrentPosition(position => console.log(position), err => console.log(err));
 	return <div>HI</div>;
 };
 
