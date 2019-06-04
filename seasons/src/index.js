67b9ee9c22301bf8d9c711ee0519adaee536f075
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-04 04:16:12 -0400

    feat: added class component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(position => console.log(position), err => console.log(err));
-	return <div>HI</div>;
-};
+
+class App extends React.Component {
+	//this is specific to javascript
+	constructor(props) {
+		super(props);
+		//initialize relevant value
+		this.state = { lat: null };
+	}
+
+	render() {
+		window.navigator.geolocation.getCurrentPosition(position => console.log(position), err => console.log(err));
+		return <div>Latitude: </div>;
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
