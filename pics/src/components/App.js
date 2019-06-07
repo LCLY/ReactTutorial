6fd96e918cf5d8dfeff0b10b3c7af5b84dc41086
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-07 16:38:18 -0400

    refactor: added 2 methods of getting data

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,13 +3,30 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 class App extends React.Component {
 	//generally the term is submitted through the Search Bar component, it goes into the prop.onSubmit, back to this.onSearchSubmit and then get the term from the function below
-	onSearchSubmit(term) {
-		axios.get('https://api.unsplash.com/search/photos', {
+	//1 way to get response/result data
+	/*onSearchSubmit(term) {
+		axios
+			.get('https://api.unsplash.com/search/photos', {
+				params: { query: term },
+				headers: {
+					Authorization: 'Client-ID 57e1fe643a74d96ba2ec915ea39670b3468aebf624ec02d3e65bb046058f3d05',
+				},
+			})
+			.then(response => {
+				console.log(response.data.results);
+			});
+	}*/
+
+	//another method: using async and await
+	async onSearchSubmit(term) {
+		const response = await axios.get('https://api.unsplash.com/search/photos', {
 			params: { query: term },
 			headers: {
 				Authorization: 'Client-ID 57e1fe643a74d96ba2ec915ea39670b3468aebf624ec02d3e65bb046058f3d05',
 			},
 		});
+
+		console.log(response.data.results);
 	}
 
 	render() {
