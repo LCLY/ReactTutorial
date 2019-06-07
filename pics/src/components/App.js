e3bad224098cad2825127691e7f3d5735d576e80
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-07 06:19:53 -0400

    refactor: added an api key

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
-
 class App extends React.Component {
 	//generally the term is submitted through the Search Bar component, it goes into the prop.onSubmit, back to this.onSearchSubmit and then get the term from the function below
 	onSearchSubmit(term) {
-		console.log(term);
+		axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization: 'Client-ID 57e1fe643a74d96ba2ec915ea39670b3468aebf624ec02d3e65bb046058f3d05',
+			},
+		});
 	}
-	//57e1fe643a74d96ba2ec915ea39670b3468aebf624ec02d3e65bb046058f3d05
 
 	render() {
 		return (
