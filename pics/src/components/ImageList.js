278ca2a5e14f26d6a58e808fd6b102a7e1e634f3
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-07 18:33:33 -0400

    refactor: showing images

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 const ImageList = props => {
-	console.log(props.images);
-	return <div>ImageList</div>;
+	/*
+    they do the same thing
+    const images = props.images.map((image) => {
+		return <img alt={image.description} key={image.id} src={image.urls.regular} />;
+    });
+    
+    const images = props.images.map(({description, id, urls}) => {
+		return <img alt={description} key={id} src={urls.regular} />;
+	});
+    */
+
+	const images = props.images.map(({ description, id, urls }) => {
+		return <img alt={description} key={id} src={urls.regular} />;
+	});
+	return <div>{images}</div>;
 };
 
 export default ImageList;
