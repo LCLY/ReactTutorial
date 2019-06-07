292baf82192086c1ee949758fa420540dc9a057a
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-07 17:27:05 -0400

    refactor: moved the baseurl and headers

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -1,2 +1,9 @@
 //all code for axios configuration
 import axios from 'axios';
+
+export default axios.create({
+	baseURL: 'https://api.unsplash.com',
+	headers: {
+		Authorization: 'Client-ID 57e1fe643a74d96ba2ec915ea39670b3468aebf624ec02d3e65bb046058f3d05',
+	},
+});
