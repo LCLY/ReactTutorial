d95defddc65fe23357dafc05862bc8fd6549a1b9
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-08 20:41:51 -0400

    refactor: used reactRef and used addEventListener

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,11 +1,32 @@
 //this is to show one image by itself
 import React from 'react';
 class ImageCard extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { span: 0 };
+		//createRef gives access to a single DOM element
+		//used to replaced document.querySelector('img')
+		this.imageRef = React.createRef();
+	}
+
+	componentDidMount() {
+		console.log(this.imageRef);
+		console.log(this.imageRef.current.clientHeight); //this line would just show 0 because we fetch the data too early
+		this.imageRef.current.addEventListener('load', this.setSpans); //callback setSpans after load
+	}
+
+	setSpans = () => {
+		console.log(this.imageRef.current.clientHeight); //after loading, the callback function will be able to retrieve data
+		const height = this.imageRef.current.clientHeight;
+		const spans = Math.ceil(height / 10);
+		this.setState({ spans });
+	};
+
 	render() {
 		const { description, urls } = this.props.image; //with this line, we can remove the this.props.image. from this.props.image.description
 		return (
-			<div>
-				<img alt={description} src={urls.regular} />
+			<div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+				<img ref={this.imageRef} alt={description} src={urls.regular} />
 			</div>
 		);
 	}
